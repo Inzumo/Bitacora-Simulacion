83ade3bb2e23c42f8aff935b7bda28c9e6b03521
Juan Manuel Posada <146497015+Inzumo@users.noreply.github.com>
2026-09-01 19:31:19 -0500

    Update parameters.js

--- Unidad 3/src/simulation/parameters.js
+++ Unidad 3/src/simulation/parameters.js
@@ -1,20 +1,106 @@
+
 import * as THREE from 'three';
 
 export function createParameters() {
+
   return {
-    timeStep: { value: 0.016 },
-    maxSpeed: { value: 10.0 },
-    particleSize: { value: 0.08 },
-    radialEnabled: { value: 0.0 },
-    radialStrength: { value: 2.0 },
-    vortexEnabled: { value: 0.0 },
-    vortexStrength: { value: 2.0 },
-    curlEnabled: { value: 0.0 },
-    curlStrength: { value: 0.0 },
-    dragEnabled: { value: 1.0 },
-    dragCoefficient: { value: 0.05 },
-    windEnabled: { value: 0.0 },
-    wind: { value: new THREE.Vector3(0, 0, 0) },
-    currentCenter: { value: new THREE.Vector3(0, 0, 0) }
+
+    // ==================================================
+    // INTEGRACIÓN
+    // ==================================================
+
+    timeStep: {
+      value: 0.016
+    },
+
+    maxSpeed: {
+      value: 10.0
+    },
+
+    // ==================================================
+    // PARTÍCULAS
+    // ==================================================
+
+    particleSize: {
+      value: 0.08
+    },
+
+    // ==================================================
+    // FUERZA RADIAL
+    // ==================================================
+
+    radialEnabled: {
+      value: 0.0
+    },
+
+    radialStrength: {
+      value: 2.0
+    },
+
+    // ==================================================
+    // FUERZA VÓRTICE
+    // ==================================================
+
+    vortexEnabled: {
+      value: 0.0
+    },
+
+    vortexStrength: {
+      value: 2.0
+    },
+
+    // ==================================================
+    // CURL
+    // ==================================================
+
+    curlEnabled: {
+      value: 0.0
+    },
+
+    curlStrength: {
+      value: 0.0
+    },
+
+    // ==================================================
+    // DRAG
+    // ==================================================
+
+    dragEnabled: {
+      value: 1.0
+    },
+
+    dragCoefficient: {
+      value: 0.05
+    },
+
+    // ==================================================
+    // VIENTO
+    // ==================================================
+
+    windEnabled: {
+      value: 0.0
+    },
+
+    wind: {
+      value:
+        new THREE.Vector3(
+          0,
+          0,
+          0
+        )
+    },
+
+    // ==================================================
+    // CENTRO DE LAS FUERZAS
+    // ==================================================
+
+    currentCenter: {
+      value:
+        new THREE.Vector3(
+          0,
+          0,
+          0
+        )
+    }
   };
 }
